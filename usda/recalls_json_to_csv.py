1382ef94855496c0da49a8bea817fd73454a5527
Jacob Harris <harrisj.home@gmail.com>
2022-04-26 21:39:55 -0400

    Remove products from recalls.csv

--- usda/recalls_json_to_csv.py
+++ usda/recalls_json_to_csv.py
@@ -7,7 +7,7 @@ import json
 def convert_ndjson_to_csv(json_path: str, csv_path: str):
     fieldnames = ['start_date', 'end_date', 'id', 'title', 'url', 'reasons', 'status', 'risk_level',
     'establishment_id', 'establishment_slug', 'establishment_name', 'establishment_address',
-    'establishment_telephone', 'establishment_grant_date', 'establishment_activities', 'products',
+    'establishment_telephone', 'establishment_grant_date', 'establishment_activities',
     'quantity_recovered', 'quantity_unit', 'states']
 
     with open(json_path, 'r') as jsonfile:
